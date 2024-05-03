dd358e7d3612a051601dd4498646c06482e39bc9
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-03 16:22:59 +0900

    add TodoBean.java

---
+++ src/main/java/beans/TodoBean.java
@@ -0,0 +1,27 @@
+package beans;
+
+public class TodoBean {
+	private int id;
+	private String taskname;
+	private String taskurl;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTaskname() {
+		return taskname;
+	}
+	public void setTaskname(String taskname) {
+		this.taskname = taskname;
+	}
+	public String getTaskurl() {
+		return taskurl;
+	}
+	public void setTaskurl(String taskurl) {
+		this.taskurl = taskurl;
+	}
+
+}
