8a47fc0191613371c77e0a55ea096cb0f98229c4
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-04 13:33:56 +0900

    add insertTodo & getTodoBean

--- src/main/java/model/TodoLogic.java
+++ src/main/java/model/TodoLogic.java
@@ -9,8 +9,60 @@ import java.util.ArrayList;
 import javax.naming.NamingException;
 
 import beans.TodoBean;
+import jakarta.servlet.http.HttpServletRequest;
 
 public class TodoLogic {
+	
+	public TodoBean getTodoBean(int id) throws SQLException, NamingException {
+		
+		TodoBean todoBean = new TodoBean();
+		
+		String sql = "select id, taskname, taskurl from todo where id=?";
+		
+		try (Connection con = ConnectionBase.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);) {
+			
+			pstmt.setInt(1, id);
+			
+			System.out.println(pstmt.toString());
+			
+			ResultSet rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				todoBean.setId(id);
+				todoBean.setTaskname(rs.getString("taskname"));
+				todoBean.setTaskurl(rs.getString("taskurl"));
+			}
+		}
+		return todoBean;
+	}
+	
+	public TodoBean getTodoBean(HttpServletRequest request) {
+		TodoBean todoBean = new TodoBean();
+		
+		todoBean.setId(0);
+		todoBean.setTaskname(request.getParameter("taskname"));
+		todoBean.setTaskurl(request.getParameter("taskurl"));
+
+		return todoBean;
+	}
+
+	public void insertTodo(TodoBean todoBean) throws SQLException, NamingException {
+
+		String sql = "insert into todo(id, taskname, taskurl) values(?,?,?);";
+		try (Connection con = ConnectionBase.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);) {
+
+			pstmt.setInt(1, todoBean.getId());
+			pstmt.setString(2, todoBean.getTaskname());
+			pstmt.setString(3, todoBean.getTaskurl());
+
+			System.out.println(pstmt.toString());
+
+			pstmt.executeUpdate();
+		}
+	}
+
 	public ArrayList<TodoBean> getAllTodo() throws SQLException, NamingException {
 
 		ArrayList<TodoBean> todoList = new ArrayList<TodoBean>();
@@ -19,19 +71,19 @@ public class TodoLogic {
 
 		try (Connection con = ConnectionBase.getConnection();
 				PreparedStatement pstmt = con.prepareStatement(sql);) {
-	
+
 			System.out.println(pstmt.toString());
-		
+
 			ResultSet rs = pstmt.executeQuery();
-			
+
 			while (rs.next()) {
-				
+
 				TodoBean todoBean = new TodoBean();
-				
+
 				todoBean.setId(rs.getInt("id"));
 				todoBean.setTaskname(rs.getString("taskname"));
 				todoBean.setTaskurl(rs.getString("taskurl"));
-				
+
 				todoList.add(todoBean);
 			}
 		}
